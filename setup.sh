9de705e8ebd43cf628fb6801565a9e28a71985c9
mvpmark <67450832+mvpmark@users.noreply.github.com>
2022-12-26 11:31:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-az group create --name RgLadder --location SouthIndia
+az group create --name RgLadder --location CentralIndia
 az vm create --resource-group RgLadder --name VMLadder01 --image UbuntuLTS --admin-username markpeng --generate-ssh-keys --size  Standard_B1s --custom-data SS.txt  
 az vm open-port --resource-group RgLadder --name VMLadder01 --port 443 --priority 1001
